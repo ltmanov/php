c631a0401978737ad1e21d6614de76499d077d17
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-08-29 11:35:52 -0400

    First Upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form>
+      <input type="text"><br />
+      <input type="text">
+    </form>
+
+  </body>
+</html>
