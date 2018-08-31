b44f67cb701bffff7488f4f9d6c511de608d5996
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-08-31 10:45:35 -0400

    .237

--- login.php
+++ login.php
@@ -3,28 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-
-
-
+//makes post variables
+    <?php
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+        ?>
+//body section
   </head>
   <body>
     <form method="post" action="">
-      <input type="text" name="username" placeholder="Enter Username..."><br />
-      <input type="password" name="password" value="" >
+      <input type="text" name="username" placeholder="Enter Username...">
+      <br />
+      <input type="password" name="password" >
       <br />
       <input type="submit" value="go">
     </form>
+//password creation
 <?php
-    $username=$_POST['username'];
-    $password=$_POST['password'];
+if (isset($username) && isset($password))
+{
+  //echo "Username was ".$username;
+  //echo "<br>";
+  //echo "Password was ".$password;
+  if ($username == "lev" && $password ="password")
+  {
+    $_SESSION['Username']=$username;
+  }
 
-    ?>
-<?php
-if (isset($username)==true){
-  echo "Username was: ".$username;
-}
-if (isset($password)==true){
-  echo "Password was: ".$password;
+  echo "Logged in as: ".$_SESSION['username'];
+
 }
 
  ?>
