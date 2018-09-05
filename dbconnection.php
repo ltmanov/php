45591737d35715b18887b2a7c2f6b8460b7d6b23
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-05 11:59:31 -0400

    .118

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php //setting up the database connection
-$db_host ='localhost'; // database is installed on php server
+$db_host = 'localhost'; // database is installed on php server
 $db_user = 'lev'; // name to login to mysql
 $db_password = 'southhills#'; // password
 $db_name = 'lev'; //name of db
