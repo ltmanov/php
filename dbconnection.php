2740a2cc7c3e6773398b17d7b4eae817817b34b0
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-05 11:58:22 -0400

    .1175

--- dbconnection.php
+++ dbconnection.php
@@ -3,7 +3,7 @@ $db_host ='localhost'; // database is installed on php server
 $db_user = 'lev'; // name to login to mysql
 $db_password = 'southhills#'; // password
 $db_name = 'lev'; //name of db
-$conn= new mysqli($db_host,$db_user,$db_password,$db_name);
+$conn = new mysqli($db_host,$db_user,$db_password,$db_name);
 if ($conn->connect_error){
   die("Connection failed: ". $conn->connect_error)
 }?>
