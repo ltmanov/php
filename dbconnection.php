6af43755ca9659e56e7d3a1a40c7de5ba490c894
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-05 11:46:00 -0400

    .11

--- dbconnection.php
+++ dbconnection.php
@@ -7,7 +7,9 @@ $conn= new mysqli($db_host,$db_user,$db_password,$db_name);
 if ($conn->connect_error){
   die("Connection failed: ". $conn->connect_error)
 }
-
+else {
+  echo "Logged in.";
+}
 
 
  ?>
