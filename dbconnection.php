d92d4ff56f7b6177d8eb99e3e27063eec7177faa
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-05 12:03:47 -0400

    .1189

--- dbconnection.php
+++ dbconnection.php
@@ -4,8 +4,8 @@ $db_user = 'lev'; // name to login to mysql
 $db_password = 'southhills#'; // password
 $db_name = 'lev'; //name of db
 $conn = new mysqli($db_host,$db_user,$db_password,$db_name);
-// if ($conn->connect_error){
-//   die("Connection failed: ". $conn->connect_error)
-// }
+if ($conn->connect_error){
+  die("Connection failed: ". $conn->connect_error);
+}
 ?>
  <p>Logged in</p>
