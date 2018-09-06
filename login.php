c072cb0e05ae2db3e5d04effb4f2672ea29a0b36
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-06 11:20:07 -0400

    a

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start(); // can use require-NEEDED or include
 require('dbconnection.php'); //die can kill this page as well
 
-if(isset($_POST['username']))
+if(!isset($_POST['username']))
   {
     $username=$_POST['username'];
     $password=$_POST['password'];
@@ -10,7 +10,7 @@ if(isset($_POST['username']))
     $sql="SELECT username, password FROM users WHERE username = $username";
     //execute the sql and return the array to $result
     $result = $conn->query($sql);   //extracting the returned query information
-    while ( $row=mysqli_fetch_assoc($result) )//$row=mysqli_fetch_assoc($result); //$row = $result->fetch_assoc()
+    while ( $row = $result->fetch_assoc()) )//$row=mysqli_fetch_assoc($result); //$row = $result->fetch_assoc()
     {//loops through all the values in the arrays
       if (($username == $row['username']) && ($password == $row['password'])
         {//row is database value
