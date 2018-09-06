80ac40f531d78cec5d3e425580cdd4ef8de66ff1
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-06 10:52:34 -0400

    .14

--- login.php
+++ login.php
@@ -1,22 +1,19 @@
 <?php
-session_start();
-// can use require-NEEDED or include
+session_start(); // can use require-NEEDED or include
 require('dbconnection.php'); //die can kill this page as well
-
-if(isset($_POST['username'])){
+if(isset($_POST['username']))
+{
   $username=$_POST['username'];
   $password=$_POST['password'];
   //sql statement to execute
-  $sql="SELECT username, password FROM users WHERE username= $username";
+  $sql="SELECT username, password FROM users WHERE username = $username";
   //execute the sql and return the array to $result
-  $result = $conn->query($sql);
-
-  //extracting the returned query information
+  $result = $conn->query($sql);   //extracting the returned query information
   while ($row as $result->fetch_assoc()) {//loops through all the values in the arrays
-    if ($username == $row['username'] && ($password == $row['password']) {
+    if (($username == $row['username']) && ($password == $row['password']) {
       //row is database value
           $_SESSION['username']=$username;
-          $_SESSION['password']=$password;
+          //$_SESSION['password']=$password;
       }
     }
 }
@@ -47,10 +44,8 @@ if(isset($_POST['username'])){
       <input type="submit" value="go">
       <input type="submit" name="logout" value="logout"><!--value is text on button, submit makes pages reload-->
     </form>
-<!--password creation-->
-<?php
-echo "Logged in as: ". $_SESSION['username'];
- ?>
+
+<?php echo "Logged in as: ". $_SESSION['username']; ?>
 
   </body>
 </html>
