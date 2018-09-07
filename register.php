04e10af3f2cf9caf9bf1239f174ed0c1309a17ad
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-07 10:50:32 -0400

    a

---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+   require('dbconnection.php');
+   $username=$_POST['username'];
+   $passowrd=$_POST['password'];
+   $sql="INSERT INTO users (username, password) VALUES ('$username','$password')";
+   $conn->query($sql);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+ <head>
+   <meta charset="utf-8">
+   <title></title>
+ </head>
+ <body>
+   <form method="post" action="">
+     <input type="text" name="username"> <br>
+     <input type="password" name="password"> <br>
+     <input type="submit">
+   </form>
+ </body>
+</html>
