08623ab3878930715bc917a6ac0097f5176b7b2e
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-11 10:36:46 -0400

    a

--- upload.php
+++ upload.php
@@ -1,14 +1,16 @@
 <?php
-if (!isset($_SESSION)){
+if (!isset($_SESSION))
+{
   session_start();
 }//checks if global variable for session is set
-if (!isset($_SESSION['username'])){
-  //die("Don't even think about it")
-  header('login.php')}
-  
+if (!isset($_SESSION['username']))
+{//die("Don't even think about it")
+  header('login.php');
+}
+
  ?>
  Upload Your File:
- <form action="" method="post">
+ <form action="" method="post" enctype="multipart/form-data">
    <input type="file" name="upload">
    <br />
    <input type="submit">
