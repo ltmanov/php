384113ce4170cb21596493a8ed36ec488a12b4e0
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-12 11:25:53 -0400

    a

--- upload.php
+++ upload.php
@@ -5,9 +5,12 @@ if (!isset($_SESSION))
 }//checks if global variable for session is set
 if (!isset($_SESSION['username']))
 {//die("Don't even think about it")
-  header('login.php');
+  header('Location: login.php');
 }
-if (!isset($_POST['upload'])){
+
+//var_dump($_FILES['upload']); - shows what variable is
+// echo "<hr />"
+if ( isset($_FILES['upload']) ) {
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES['upload']['name']);
 move_uploaded_file($_FILES['upload']['name'], $target_file);
