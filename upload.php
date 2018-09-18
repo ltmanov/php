0a45dc96c12c4003f1d46b2114b32025ec4c6df4
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-18 10:19:34 -0400

    a

--- upload.php
+++ upload.php
@@ -11,6 +11,12 @@ if (!isset($_SESSION['username']))
 var_dump($_FILES['upload']);// shows what variable is
 // echo "<hr />"
 if ( isset($_FILES['upload']) ) {
+//<php $folder = date("Ymd");mkdir ($folder, 0755);
+if (mkdir("uploads/", 0777, true)) {
+    
+}
+
+mkdir()
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES['upload']['name']);//location to put
 $uploadVerification=true;
