f28f0059b5e91b16669a7ab2e62fa52626679c2d
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-19 11:45:02 -0400

    a

--- upload.php
+++ upload.php
@@ -17,7 +17,7 @@ if ( isset($_FILES['upload']) ) {
   }
 
 if (!file_exists("uploads/" . $_SESSION['username'])) {
-  mkdir("uploads" . $_SESSION['username']);
+  mkdir("uploads" . $_SESSION['username'],0777,true);
 }
 
 $target_dir = "uploads/" . $_SESSION['username'] . "/";
@@ -28,23 +28,23 @@ $uploadVerification=true;
 if (file_exists($target_file)) {  $uploadVerification=false;  $ret = "Sorry file already exists";}
 
 //$finfo = finfo_open(FILEINFO_MIME_TYPES);
-$file_type = $_FILES['upload']['tmp_file'];
-switch ($file_type) {
+$file_type = $_FILES['upload']['type'];
+switch ($file_type){
   case "image/jpeg":
-    $uploadVerification=true;
+    $uploadVerification = true;
     break;
   case "image/png":
-    $uploadVerification=true;
+    $uploadVerification = true;
     break;
   case "image/gif":
-    $uploadVerification=true;
+    $uploadVerification = true;
     break;
   case "application/pdf":
-    $uploadVerification=true;
+    $uploadVerification = true;
     break;
   default:
-    $uploadVerification=false;
-    $ret = "Sorry only jpg, png, gif, pdf files are allowed.";
+    $uploadVerification = false;
+    $ret = "Sorry only jpg, png, gif, and pdf files are allowed";
 }
 
 if ($_FILES['upload']['size'] > 1000000){ $uploadVerification=false; $ret = "Sorry file is too big"; }
@@ -72,35 +72,81 @@ if ($uploadVerification){move_uploaded_file($_FILES['upload']['tmp_name'], $targ
 
 
 <!--
-/////////////////////////////////////////////////////////////////////////////////
 
- <?php
- if (!isset($_SESSION)){
-   session_start();
- }
+if (!isset($_SESSION)){
+  session_start();
+}
 
- if (!isset($_SESSION['username'])){
-   header('Location: login.php');
- }
+if (!isset($_SESSION['username'])){
+  header('Location: login.php');
+}
 
- var_dump($_POST['upload']);
- echo "<hr />";
- var_dump($_FILES['upload']);
+var_dump($_POST['upload']);
+echo "<hr />";
+var_dump($_FILES['upload']);
 
- if ( isset($_FILES['upload']) ) {
+if ( isset($_FILES['upload']) ) {
+  //check to see if uploads folder exists
+  if(!file_exists("uploads")){
+    //if uploads folder does not exists create it
+    mkdir("./uploads");
+  }
 
-   $target_dir = "uploads/";
-   $target_file = $target_dir . basename($_FILES['upload']['name']);
-   move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
+  if(!file_exists("uploads/" . $_SESSION['username'])) {
+    mkdir("uploads/" . $_SESSION['username']);
+  }
 
- }
+  $target_dir = "uploads/" . $_SESSION['username'] . "/";
+  $target_file = $target_dir . basename($_FILES['upload']['name']);
+  $uploadVerification = true;
+
+  //Lets check to see if the file already exists
+  if(file_exists($target_file)){
+    $uploadVerification = false;
+    $ret = "Sorry file already exists";
+  }
+
+  //Check file for type
+  $file_type = $_FILES['upload']['type'];
+  switch ($file_type){
+    case "image/jpeg":
+      $uploadVerification = true;
+      break;
+    case "image/png":
+      $uploadVerification = true;
+      break;
+    case "image/gif":
+      $uploadVerification = true;
+      break;
+    case "application/pdf":
+      $uploadVerification = true;
+      break;
+    default:
+      $uploadVerification = false;
+      $ret = "Sorry only jpg, png, gif, and pdf files are allowed";
+  }
 
 
- ?>
+  if($_FILES['upload']['size'] > 1000000 ){
+    $uploadVerification = false;
+    $ret = "Sorry file is too big";
+  }
 
- Upload your file.
- <form action="" method="post" enctype="multipart/form-data">
-   <input type="file" name="upload" >
-   <br />
-   <input type="submit">
- </form> -->
+
+  if ($uploadVerification) {
+    move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
+  }
+}
+?>
+Upload your file.
+<form action="" method="post" enctype="multipart/form-data">
+  <input type="file" name="upload" >
+  <br />
+  <input type="submit">
+</form>
+
+<h5 style="color:red;">
+  <?php if ($ret) { echo $ret; } ?>
+</h5>
+
+-->
