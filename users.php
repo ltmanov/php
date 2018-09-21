1b5d9d75f4ab88f48b5b22aa6668a33a527a0789
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-21 10:42:47 -0400

    a

--- users.php
+++ users.php
@@ -45,7 +45,7 @@ while ($row = $result->fetch_assoc())
     echo "<td>" . $row['password'] . "</td>";
     echo "<td>
                   <form action=\"\" method=\"post\">
-                    <input type=\"hidden\" value=\"$row[userid]\">
+                    <input name=\"id\" type=\"hidden\" value=\"" . $row[userid] . "\">
                     <input type=\"submit\" value=\"Delete\">
                   </form>
             </td>";
