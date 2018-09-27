ce317b6f591d6f1e07ea765f6fb19b01a02b518b
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-27 11:58:38 -0400

    a

--- login.php
+++ login.php
@@ -15,6 +15,7 @@ if(isset($_POST['username']))
         if (($username == $row['username']) && password_verify($password, $row['password']))
          {//row is database value
            $_SESSION['username'] = $username;//used to authenticate our session to stay logged in;
+           require('nav.php');
          }
     }//
   }
