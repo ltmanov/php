8cb974c4cb4b5e41510827803cce65d8367969d0
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-27 12:12:19 -0400

    a

--- register.php
+++ register.php
@@ -1,14 +1,14 @@
 <?php
-require('nav.php');
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
    require('dbconnection.php');
-   require('nav.php');
    $username=$_POST['username'];
    $password=$_POST['password'];
    $password=password_hash($password, PASSWORD_BCRYPT);//5.5 and higher for this function
    $sql="INSERT INTO users (username, password) VALUES ('$username','$password')";
    $conn->query($sql);
+   require('nav.php');
 }
 //ubuntu 16.04 - 5.6 ; 18.04 - 7.2; redhat and centos is still stuck on 5.4
 ?>
