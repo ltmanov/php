3ddd7c9338e70fd5f54acd302cdb72d6b972262f
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-09-27 11:47:11 -0400

    a

---
+++ nav.php
@@ -0,0 +1,14 @@
+<<?php
+if (!isset($_SESSION))
+{
+  session_start();
+}//checks if global variable for session is set
+if (!isset($_SESSION['username']))//check to see session is started
+{
+  header('Location: login.php');
+}
+ ?>
+
+ <a href="register.php">Register</a>
+ <a href=\"upload.php\">| Upload</a>
+ <a href=\"users.php\">| Users</a>";
