69bfcc7126ee0fd4f4dcac4b62558c0cd13c95f4
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-03 08:30:20 -0400

    a

--- nav.php
+++ nav.php
@@ -9,15 +9,15 @@ if (!isset($_SESSION['username']))//check to see session is started
 }
 
 echo (basename($_SERVER['PHP_SELF'])) == users.php)
-? "<a href=users.php><strong>Users</strong></a>"
-: "<a href=users.php>Users</a>" ;
+? \"<a href=users.php><strong>Users</strong></a>\"
+: \"<a href=users.php>Users</a>\" ;
 
-
+?><?php
 
  ?>
   <br />
  <a href="register.php">| Register</a>
  <a href="upload.php">| Upload</a>
- 
+
  <a href="login.php">| Log In</a>
  <br />
