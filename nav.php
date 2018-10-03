c03777f53fcd903f36d332ed694c73f1ef79979d
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-03 08:48:35 -0400

    a

--- nav.php
+++ nav.php
@@ -1,25 +1,32 @@
 <?php
-if (!isset($_SESSION))
-{
-  session_start();
-}//checks if global variable for session is set
 if (!isset($_SESSION['username']))//check to see session is started
 {
   header('Location: login.php');
+}
+//if tree for different pages
+if (basename($_SERVER['PHP_SELF']) == "register.php") {
+  echo "<a href=users.php><strong> | Register</strong></a>";
+} else {
+  echo "<a href=users.php> | Register</a>";
 }
 
 if (basename($_SERVER['PHP_SELF']) == "users.php") {
   echo "<a href=users.php><strong> | Users</strong></a>";
 } else {
-  echo "<a href=users.php>Users</a>";
+  echo "<a href=users.php> | Users</a>";
+}
+
+if (basename($_SERVER['PHP_SELF']) == "upload.php") {
+  echo "<a href=users.php><strong> | Upload</strong></a>";
+} else {
+  echo "<a href=users.php> | Upload</a>";
+}
+
+if (basename($_SERVER['PHP_SELF']) == "login.php") {
+  echo "<a href=users.php><strong> | Login</strong></a>";
+} else {
+  echo "<a href=users.php> | Login</a>";
 }
 
 
-
-
 ?>
-  <br />
- <a href="register.php">| Register</a>
- <a href="upload.php">| Upload</a>
- <a href="login.php">| Log In</a>
- <br />
