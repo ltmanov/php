b3df72bf7d8966384afdeadd924365d40d5264a5
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-03 09:42:03 -0400

    a

--- nav.php
+++ nav.php
@@ -2,33 +2,35 @@
 if (!isset($_SESSION['username']))//check to see session is started
 {
   header('Location: login.php');
-}
-?>
-<br />
+  ?>
+
+  <br />
+
 <?php
-//if tree for different pages
-if (basename($_SERVER['PHP_SELF']) == "register.php") {
-  echo "<a href=register.php><strong> | Register</strong></a>";
-} else {
-  echo "<a href=register.php> | Register</a>";
-}
+  //if tree for different pages
+  if (basename($_SERVER['PHP_SELF']) == "register.php") {
+    echo "<a href=register.php><strong> | Register</strong></a>";
+  } else {
+    echo "<a href=register.php> | Register</a>";
+  }
 
-if (basename($_SERVER['PHP_SELF']) == "users.php") {
-  echo "<a href=users.php><strong> | Users</strong></a>";
-} else {
-  echo "<a href=users.php> | Users</a>";
-}
+  if (basename($_SERVER['PHP_SELF']) == "users.php") {
+    echo "<a href=users.php><strong> | Users</strong></a>";
+  } else {
+    echo "<a href=users.php> | Users</a>";
+  }
 
-if (basename($_SERVER['PHP_SELF']) == "upload.php") {
-  echo "<a href=upload.php><strong> | Upload</strong></a>";
-} else {
-  echo "<a href=upload.php> | Upload</a>";
-}
+  if (basename($_SERVER['PHP_SELF']) == "upload.php") {
+    echo "<a href=upload.php><strong> | Upload</strong></a>";
+  } else {
+    echo "<a href=upload.php> | Upload</a>";
+  }
 
-if (basename($_SERVER['PHP_SELF']) == "login.php") {
-  echo "<a href=login.php><strong> | Login</strong></a>";
-} else {
-  echo "<a href=login.php> | Login</a>";
+  if (basename($_SERVER['PHP_SELF']) == "login.php") {
+    echo "<a href=login.php><strong> | Login</strong></a>";
+  } else {
+    echo "<a href=login.php> | Login</a>";
+  }
 }
-?>
+  ?>
 <br />
