9d1b0782b96cb2d5eb6a66531d22609402eaf1ac
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-04 10:42:14 -0400

    a

--- cookies.php
+++ cookies.php
@@ -15,12 +15,14 @@ $cookie_value="bob";
     <?php
       if (isset($_COOKIE['user'])) {
         echo "You have been here before.";
-
+        setcookie($cookie_name,$cookie_value, time() - (86400*6), "/"); 
       }
       else {
         echo "This is your first time here.";
-      //setcookie($cookie_name,$cookie_value, time() + (86400*30), "/");  
+
+      //setcookie($cookie_name,$cookie_value, time() + (86400*30), "/");
       //php7 allows this go work, otherwise would have to be up to before any html code
+      //to see cookies in chrome-> settings to advanced settings to content settings to see all cookies
       }
      ?>
 
