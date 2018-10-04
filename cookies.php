06e70ac2538b2104806a114826198f5ab24c410e
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-04 10:14:29 -0400

    a

---
+++ cookies.php
@@ -0,0 +1,26 @@
+<?php
+//setcookie(name, value, expire, path, domain, secure, httponly);
+$cookie_name ="user";
+$cookie_value="bob";
+//setcookie($cookie_name,$cookie_value, time() + (86400*30), "/");
+//86400 = 1 day
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      if (isset($_COOKIE['user'])) {
+        echo "You have been here before.";
+      }
+      else {
+        echo "This is your first time here.";
+      }
+     ?>
+
+  </body>
+</html>
