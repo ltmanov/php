ef18d1c597a0fc297f9f2717bc7b329a827465f2
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-05 10:47:21 -0400

    a

--- cookies.php
+++ cookies.php
@@ -16,32 +16,3 @@ else {
 }
 
  ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-
-  </body>
-</html>
-
-<!-- <?php
-//setcookie(name, value, expire, path, domain, secure, httponly);
-
-  if (isset($_COOKIE['user'])) {
-
-    //setcookie($cookie_name,$cookie_value, time() - (60), "/");
-    // going back in time, makes an expired cookie, which browser will delete
-  }
-  else {//make the cookie
-
-
-  //setcookie($cookie_name,$cookie_value, time() + (86400*30), "/");
-  //php7 allows this go work, otherwise would have to be up to before any html code
-  //to see cookies in chrome-> settings to advanced settings to content settings to see all cookies
-  }
- ?> -->
