e5066a9257bef8707272cbdb0458d21079b2eaf3
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-05 10:54:08 -0400

    a

--- cookies.php
+++ cookies.php
@@ -12,8 +12,8 @@ else {
   echo 'Now:       '. date('Y-m-d') ."\n";
   echo "This is your first time here. We use cookies, we are required to let you know that we use cookies.";
   setcookie($cookie_name, $cookie_value, time() + (86400*30), "/");
-  echo $_COOKIE[$cookie_name];
-  echo $_COOKIE[$cookie_value];
+  echo $_COOKIE['name'];
+  echo $_COOKIE['value'];
 }
 
  ?>
