c410ce8190b5e8fc8e680e2b0a607a62324d7350
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-05 11:26:25 -0400

    a

--- cookies.php
+++ cookies.php
@@ -3,12 +3,12 @@ $cookie_name ="user";
 $cookie_value = "".date('m-d-Y')."";
 
 if (isset($_COOKIE[$cookie_name])){
-    echo "Your last visit was:  " .$_COOKIE[$cookie_name];
+    echo "Your last visit was on:  " .$_COOKIE[$cookie_name];
     $cookie_value = "".date('m-d-Y')."";
     setcookie($cookie_name, $cookie_value, time() + (86400*30), "/");
 }
 else {
-  echo "This is your first time here. We use cookies, we are required to let you know that we use cookies.";
+  echo "This is your first time here. We are required to let you know that we use cookies.";
   setcookie($cookie_name, $cookie_value, time() + (86400*30), "/");
 }
  ?>
