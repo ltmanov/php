1c712837af035420f3d62bf6b63555bb34dd19f6
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-10 11:27:21 -0400

    a

--- shellexec.php
+++ shellexec.php
@@ -2,6 +2,11 @@
 $output = shell_exec('ls -lah');
 echo "<pre>$output</pre>";
 
+if (!stripos($ouput,"test"))
+{
+  echo = "test does not exist"
+}
+
 $pwd = shell_exec('pwd');
 echo "<pre>$pwd</pre>";
 
