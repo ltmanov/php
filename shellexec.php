92321f67b58f6e97f8db441f6d2af71a31321d48
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-11 10:19:15 -0400

    a

--- shellexec.php
+++ shellexec.php
@@ -1,14 +1,21 @@
 <?php
 $output = shell_exec('ls -lah');
-echo "<pre>$output</pre>";
-
-if (strpbrk($output,"test")==FALSE)
-{
-  echo = "test does not exist";
-}
-
+echo "$output";
 
 $pwd = shell_exec('pwd');
 echo "<pre>$pwd</pre>";
+
+$file_test = file_exists("test");
+  if ($file_test) {
+    $folder_test = is_dir("test");
+    if ($folder_test) {
+      echo "test exists, and is a folder";
+    } else {
+      echo "test exists and is a file";
+    }
+  } else {
+    mkdir("test");
+  }
+
 
 ?>
