58853fef12cf37d4cd93af1650c8d6027151cb28
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-16 10:22:51 -0400

    a

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -1,3 +1,26 @@
+<?php //setting up the database connection
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$db_host = 'localhost'; // database is installed on php server
+	$db_user = 'lev'; // name to login to mysql
+	$db_password = 'southhills#'; // password
+	$db_name = 'lev'; //name of db
+	$conn = new mysqli($db_host,$db_user,$db_password,$db_name);
+	if ($conn->connect_error){ die("Connection failed: ". $conn->connect_error);}
+  $username=$_POST['username'];
+  $password=$_POST['password'];
+  $password=password_hash($password, PASSWORD_BCRYPT);//5.5 and higher for this function
+  $sql="INSERT INTO users (username, password) VALUES ('$username','$password')";
+  $conn->query($sql);
+  //header('Location: login.php');
+}
+
+if (isset($_SESSION['username']))
+{
+  //require('nav.php');
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
