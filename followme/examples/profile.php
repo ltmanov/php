f5a1ce4139d3eb4663ae75893d80a81ee3ada7b0
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-22 11:31:12 -0400

    a

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -44,8 +44,7 @@ session_start();
 					<div class="collapse navbar-collapse" id="navbarToggler">
             <ul class="navbar-nav ml-auto">
 							 <li class="nav-item">	<a href="login.php" class="nav-link">Log In</a> </li>
-							  <li class="nav-item">	<a href="#" class="nav-link">#</a> </li>
-								<?php echo $_SESSION['email']; ?>
+							  <li class="nav-item">	<a href="#" class="nav-link"><?php echo $_SESSION['email']; ?></a></li>
             </ul>
 			    </div>
 		</div>
