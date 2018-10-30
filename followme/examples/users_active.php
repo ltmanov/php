ac24eb3cced21bc9f96bc8a70e4060a3b21bfc3c
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-30 11:20:54 -0400

    a

--- followme/examples/users_active.php
+++ followme/examples/users_active.php
@@ -89,6 +89,8 @@ while ($row = $result->fetch_assoc()) {
 		</div>
 <div class="col-md-7 col-sm-4  ml-auto mr-auto">
 	<h6> <?php echo ( $_user_firstname . " " . $_user_lastname. " user id:  " . $_user_id) ?><br/><small><?php echo $_user_title ?></small></h6>
+<?php checkUser($_user_id); ?>
+
 </div>
 <div class="col-md-3 col-sm-2  ml-auto mr-auto">
 	<div class="form-check">
