8985011f4e77f7382b7b553f65248491f5ab76b5
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-30 08:43:22 -0400

    a

--- followme/examples/users_active.php
+++ followme/examples/users_active.php
@@ -6,6 +6,7 @@ $db_password = 'southhills#';
 $db_name = 'lev';
 $conn = new mysqli($db_host,$db_user,$db_password,$db_name);
 if ($conn->connect_error){ die("Connection failed: ". $conn->connect_error);}
+echo $_SESSION('userid');
 ?>
 
 <!doctype html>
@@ -86,11 +87,11 @@ while ($row = $result->fetch_assoc()) {
 			$conn2 = new mysqli($db_host,$db_user,$db_password,$db_name);
 			$sql2="SELECT * FROM fm_follow";
 			$result2 = $conn2->query($sql2);
-			echo "checked";
-			while ($row2 = $result2->fetch_assoc()) {
-				if (($_user_id == $row2['follow_by']))
-				{
 
+			while ($row2 = $result2->fetch_assoc()) {
+				if (($_user_id == $row2['follow_by']) && ($_SESSION['userid']==$row2['user_id']))
+				{
+					echo "checked";
 				}// && ($row2['user_id']==$_SESSION['user_id'])
 			}
 
