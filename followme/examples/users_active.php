b8c72da6871215812339e561cbe9f5281d8ea12a
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-10-30 09:44:41 -0400

    a

--- followme/examples/users_active.php
+++ followme/examples/users_active.php
@@ -8,14 +8,16 @@ $conn = new mysqli($db_host,$db_user,$db_password,$db_name);
 if ($conn->connect_error){ die("Connection failed: ". $conn->connect_error);}
 echo $_SESSION['userid'];
 
+$conn2 = new mysqli($db_host,$db_user,$db_password,$db_name);
+$sql2="SELECT * FROM fm_follow";//user_id=" . $_SESSION['userid'];
+$result2 = $conn2->query($sql2);
+while ($row2 = $result2->fetch_assoc()) {
+	echo "checked";
+}
+
 function checkUser()
 {
-	$conn2 = new mysqli($db_host,$db_user,$db_password,$db_name);
-	$sql2="SELECT * FROM fm_follow";//user_id=" . $_SESSION['userid'];
-	$result2 = $conn2->query($sql2);
-	while ($row2 = $result2->fetch_assoc()) {
-		echo "hidden";
-	}
+
 }
 ?>
 
