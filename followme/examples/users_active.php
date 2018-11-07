a16facef9c7d360681f4bde6b1267b82f18d0467
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-07 09:12:11 -0500

    a

--- followme/examples/users_active.php
+++ followme/examples/users_active.php
@@ -25,13 +25,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 		foreach($_POST as $key => $value2)
 		{
 			$found=0;
-			if ($value1 == $value2){ echo " This value: ".$value1." is in! <br />"; $found=1;
+			if ($value1 == $value2){// echo " This value: ".$value1." is in! <br />";
+			$found=1;
 			$sql3 ="INSERT INTO fm_follow (user_id, follow_by) VALUES ($main_user,$value1)";
 			$result3 = $conn->query($sql3);
 			break;
 			}
 		}
-		if ($found==0){echo "This value: ".$value1." is NOT FOUND! <br />";
+		if ($found==0){//echo "This value: ".$value1." is NOT FOUND! <br />";
 			$sql4 ="DELETE FROM fm_follow WHERE user_id='$main_user' AND follow_by='$value1'";
 			$result4 = $conn->query($sql4);
 
