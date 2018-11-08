e68a34316289bd575fc58420c33b88f568f779fb
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-08 10:26:56 -0500

    a

---
+++ followme/examples/dbconnection.php
@@ -0,0 +1,12 @@
+<?php //setting up the database connection
+$db_host = 'localhost'; // database is installed on php server
+$db_user = 'lev'; // name to login to mysql
+$db_password = 'southhills#'; // password
+$db_name = 'lev'; //name of db
+$conn = new mysqli($db_host,$db_user,$db_password,$db_name);
+if ($conn->connect_error){
+  die("Connection failed: ". $conn->connect_error);
+}
+else{echo "Connection  Success";}
+?>
+<br>
--- followme/examples/users_active.php
+++ followme/examples/users_active.php
@@ -1,58 +1,46 @@
 <?php
 session_start();
-$db_host = 'localhost';
-$db_user = 'lev';
-$db_password = 'southhills#';
-$db_name = 'lev';
-$conn = new mysqli($db_host,$db_user,$db_password,$db_name);
-if ($conn->connect_error){ die("Connection failed: ". $conn->connect_error);}
+require('dbconnection.php');
 
 $main_user = $_SESSION['userid'];
 
-
 $sql="SELECT * FROM fm_users";
-$result = $conn->query($sql);
+$result = $conn->query($sqly);
 $all_user=array();
-while ($row = $result->fetch_assoc())
-{
-	$all_user[]=$row['userid'];
-}
+
+while ($row = $result->fetch_assoc()){	$all_user[]=$row['userid'];}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	//var_dump($all_user);
 	foreach ($all_user as $value1) {
 		foreach($_POST as $key => $value2)
-		{
-			$found=0;
-			if ($value1 == $value2){// echo " This value: ".$value1." is in! <br />";
-			$found=1;
-			$sql3 ="INSERT INTO fm_follow (user_id, follow_by) VALUES ($main_user,$value1)";
-			$result3 = $conn->query($sql3);
-			break;
+			{
+				$found=0;
+				if ($value1 == $value2)
+				{// echo " This value: ".$value1." is in! <br />";
+					$found=1;
+					$sql3 ="INSERT INTO fm_follow (user_id, follow_by) VALUES ($main_user,$value1)";
+					$result3 = $conn->query($sql3);
+					break;
+				}
 			}
-		}
-		if ($found==0){//echo "This value: ".$value1." is NOT FOUND! <br />";
+		if ($found==0)
+			{//echo "This value: ".$value1." is NOT FOUND! <br />";
 			$sql4 ="DELETE FROM fm_follow WHERE user_id='$main_user' AND follow_by='$value1'";
 			$result4 = $conn->query($sql4);
-
 		}
 	}
 }
 
-
-
 $user_data=array();
 $sql2 = "SELECT * FROM fm_follow WHERE user_id = " . $_SESSION['userid'];
 $result2 = $conn->query($sql2);
-while ($row2 = $result2->fetch_assoc())
-{
-		$user_data[]=$row2['follow_by'];
-}
-//var_dump($user_data);
-function checkUser($user,$user_data )
-{//this handles the checking to make sure users are properly displayed when following
-  if (in_array("$user", $user_data)) {echo "checked";}// else {echo "checked";}
+
+while ($row2 = $result2->fetch_assoc()){$user_data[]=$row2['follow_by'];}
+
+function checkUser($user,$user_data ){if (in_array("$user", $user_data)) {echo "checked";}
+
 }
 ?>
 <!doctype html>
