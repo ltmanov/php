ef29056874da33946748b20c0794c659083fe245
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-08 10:31:11 -0500

    a

--- followme/examples/users_active.php
+++ followme/examples/users_active.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
-require('dbconnection.php');
+$db_host = 'localhost';
+$db_user = 'lev';
+$db_password = 'southhills#';
+$db_name = 'lev';
+$conn = new mysqli($db_host,$db_user,$db_password,$db_name);
+if ($conn->connect_error){ die("Connection failed: ". $conn->connect_error);}
 
 $main_user = $_SESSION['userid'];
 
