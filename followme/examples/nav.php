db54728612458d5b60a2ecff3606472c573fe5c7
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-09 10:29:17 -0500

    a

--- followme/examples/nav.php
+++ followme/examples/nav.php
@@ -1,18 +1,3 @@
-?>
-
 <li class="nav-item">	<a href="login.php" class="nav-link">Log In</a> </li>
 <li class="nav-item">	<a href="users_active.php" class="nav-link">Followers</a> </li>
 <li class="nav-item">	<a href="profile.php" class="nav-link"><?php echo $_SESSION['email']; ?></a></li>
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
