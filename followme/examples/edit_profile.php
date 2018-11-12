bb6211c84f76d7d39238f7e1a7be5bd288343171
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-12 11:27:55 -0500

    a

--- followme/examples/edit_profile.php
+++ followme/examples/edit_profile.php
@@ -1,13 +1,7 @@
 <?php
 session_start();
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{//connection setup
-	$db_host = 'localhost'; // database is installed on php server
-	$db_user = 'lev'; // name to login to mysql
-	$db_password = 'southhills#'; // password
-	$db_name = 'lev'; //name of db
-	$conn = new mysqli($db_host,$db_user,$db_password,$db_name);
-	if ($conn->connect_error){ die("Connection failed: ". $conn->connect_error);}
+require('dbconnection.php');
+
   $sql ="UPDATE fm_users SET firstname='".$_POST['firstname']."', lastname='".$_POST['lastname']."',
   title='".$_POST['title']."', descr='".$_POST['descr']."' WHERE userid = " . $_SESSION['userid'];
   $result = $conn->query($sql);
