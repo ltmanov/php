c94b05de47d5fd1cb9416e8a5b0347765492ec7b
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-12 11:24:34 -0500

    a

--- followme/examples/edit_profile.php
+++ followme/examples/edit_profile.php
@@ -126,6 +126,71 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 <input type="text" name="title" class="form-control" placeholder="<?php echo $_SESSION['title']; ?>">
 </div>
 
+<?php
+if (!isset($_SESSION))
+{
+  session_start();
+}//checks if global variable for session is set
+if (!isset($_SESSION['username']))
+{//die("Don't even think about it")
+  header('Location: login.php');
+}
+require('nav.php');
+
+var_dump($_FILES['upload']);// shows what variable is
+// echo "<hr />"
+if ( isset($_FILES['upload']) ) {
+  if ( !file_exists("uploads")){
+    //if uploads doesnt exist, make it
+    mkdir("./uploads");
+  }
+
+if (!file_exists("uploads/" . $_SESSION['username'])) {
+  mkdir("uploads/" . $_SESSION['username'],0777,true);
+}
+
+$target_dir = "uploads/" . $_SESSION['username'] . "/";
+$target_file = $target_dir . basename($_FILES['upload']['name']);//location to put
+$uploadVerification=true;
+
+//check to see if file exists
+if (file_exists($target_file)) {  $uploadVerification=false;  $ret = "Sorry file already exists";}
+
+//$finfo = finfo_open(FILEINFO_MIME_TYPES);
+$file_type = $_FILES['upload']['type'];
+switch ($file_type){
+  case "image/jpeg":
+    $uploadVerification = true;
+    break;
+  case "image/png":
+    $uploadVerification = true;
+    break;
+  case "image/gif":
+    $uploadVerification = true;
+    break;
+  case "application/pdf":
+    $uploadVerification = true;
+    break;
+  default:
+    $uploadVerification = false;
+    $ret = "Sorry only jpg, png, gif, and pdf files are allowed";
+}
+
+if ($_FILES['upload']['size'] > 1000000){ $uploadVerification=false; $ret = "Sorry file is too big"; }
+
+if ($uploadVerification){move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);}
+}
+ ?>
+
+ Upload Your File:
+ <form action="" method="post" enctype="multipart/form-data">
+   <input type="file" name="upload">
+   <br />
+   <input type="submit">
+ </form>
+
+<h5 style="color:red;"> <?php if($ret){echo $ret;}  ?> </h5>
+
 
 <label>Message</label>
 <textarea class="form-control" name="message" rows="4" placeholder="<?php echo $_SESSION['descr']; ?>"></textarea>
