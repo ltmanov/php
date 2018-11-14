60a78eca8443d2cf758cd0ad89b900d44f3d0771
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-14 14:32:12 -0500

    a

--- followme/examples/nav.php
+++ followme/examples/nav.php
@@ -1,4 +1,20 @@
 <li class="nav-item">	<a href="login.php" class="nav-link">Log In</a> </li>
 <li class="nav-item">	<a href="edit_profile.php" class="nav-link">Settings</a> </li>
 <li class="nav-item">	<a href="users_active.php" class="nav-link">Followers</a> </li>
-<li class="nav-item">	<a href="profile.php" class="nav-link"><?php echo $_SESSION['email']; ?></a></li>
+<li class="nav-item">	<a href="profile.php" class="nav-link">View Profile</a></li>
+<li class="nav-item">	<a href="#" class="nav-link">Log Out</a></li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <li class="nav-item">	<a href="profile.php" class="nav-link"><?php echo $_SESSION['email']; ?></a></li> -->
