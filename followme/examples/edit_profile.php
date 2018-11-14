7eb0e22e839dbb8acbd582d320e8fea171a0fc78
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-14 12:59:15 -0500

    a

--- followme/examples/edit_profile.php
+++ followme/examples/edit_profile.php
@@ -1,21 +1,19 @@
 <?php
 session_start();
+require('dbconnection.php');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-	require('dbconnection.php');
-
-	if (isset($_FILES['upload']) ) {
-
+	if (isset($_FILES['upload']) )
+	{
 		$target_dir = "images/";
 		$target_file = $target_dir . basename($_FILES['upload']['name']);
 		$uploadVerification=true;
-
 		//if (file_exists($target_file)) {  $uploadVerification=false;  $ret = "Sorry file already exists";}
-
 		$file_type = $_FILES['upload']['type'];
 
-		switch ($file_type){
+		switch ($file_type)
+		{
 			case "image/jpeg":
 				$uploadVerification = true;
 				break;
@@ -32,19 +30,21 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 
 		if ($_FILES['upload']['size'] > 1000000){ $uploadVerification=false; $ret = "Sorry file is too big"; }
 
-		if ($uploadVerification){move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
+		if ($uploadVerification)
+		{move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
 			//adds sql UPDATE
 			//$file_name = basename($_FILES['upload']['tmp_name']);
 			$sql2 ="UPDATE fm_users SET image='$target_file' WHERE userid = " . $_SESSION['userid'];
 			$result2 = $conn->query($sql2);
-			while ($row2 = $result2->fetch_assoc()) {
+			while ($row2 = $result2->fetch_assoc())
+			{
 				if (($_SESSION['userid'] == $row2['userid']))
 				{
 					$_SESSION['image'] = $row2['image'];
 				}
 			}
 		}
-}
+	}
 
   $sql_u ="UPDATE fm_users SET firstname='".$_POST['firstname']."', lastname='".$_POST['lastname']."',
   title='".$_POST['title']."', descr='".$_POST['descr']."' WHERE userid = " . $_SESSION['userid'];
@@ -53,7 +53,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
   $sql="SELECT * FROM fm_users WHERE userid = " . $_SESSION['userid'];
   $result = $conn->query($sql);
   while ($row = $result->fetch_assoc()) {
-    if (($_SESSION['userid'] == $row['userid']))
+    if ($_SESSION['userid'] == $row['userid'])
 		{
       $_SESSION['firstname'] = $row['firstname'];
 			$_SESSION['lastname'] = $row['lastname'];
