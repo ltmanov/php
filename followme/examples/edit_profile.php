d13e159de761bff32204157c9b50db712d2b7873
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-16 12:14:17 -0500

    a

--- followme/examples/edit_profile.php
+++ followme/examples/edit_profile.php
@@ -37,13 +37,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 		if ($_FILES['upload']['size'] > 1000000){ $uploadVerification=false; $ret = "Sorry file is too big"; }
 
 		$target_file = $target_dir . $_SESSION['userid'] . $img_type; //NEW
-		if ($uploadVerification){move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);}
+		if ($uploadVerification){move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
 
-		if (!$uploadVerification){
-		$sql2 ="UPDATE fm_users SET image='$target_file' WHERE userid = " . $_SESSION['userid'];
-		$result2 = $conn->query($sql2);
+			$sql2 ="UPDATE fm_users SET image='$target_file' WHERE userid = " . $_SESSION['userid'];
+			$result2 = $conn->query($sql2);
+
 		}
-		
+
+
+
+
+
 		$sql_u ="UPDATE fm_users SET firstname='".$_POST['firstname']."', lastname='".$_POST['lastname']."',title='".$_POST['title']."', descr='".$_POST['descr']."' WHERE userid = " . $_SESSION['userid'];
 		$result_update = $conn->query($sql_u);
 
